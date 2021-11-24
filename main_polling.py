0dadc881463a31b0ddb26c6243a92ef8bcd7b186
cpython-projects <74833506+cpython-projects@users.noreply.github.com>
2021-11-24 20:48:56 +0200

    Add files via upload

---
+++ main_polling.py
@@ -0,0 +1,90 @@
+from config import TOKEN
+import telebot
+
+bot = telebot.TeleBot(TOKEN)
+
+with open('courses.txt') as file:
+    courses = [item.split(',') for item in file]
+
+with open('schedule.txt') as file:
+    courses_plan = {
+        'start': [],
+        'pro': [],
+        'other': [],
+    }
+
+    for item in file:
+        if 'start' in item.lower():
+            courses_plan['start'].append(item)
+        elif 'pro' in item.lower():
+            courses_plan['pro'].append(item)
+        else:
+            courses_plan['other'].append(item)
+
+
+
+@bot.message_handler(commands=['start'])
+def message_start(message):
+    bot.send_message(message.chat.id, 'Hello, user!')
+
+
+@bot.message_handler(commands=['help'])
+def help_message(message):
+    res = '/courses - list of courses\n' \
+          '/schedule - schedule of courses,'
+    bot.reply_to(message, res)
+
+
+@bot.message_handler(commands=['courses'])
+def list_of_courses(message):
+    keyboard = telebot.types.InlineKeyboardMarkup(row_width=1)
+
+    for text, url in courses:
+        url_button = telebot.types.InlineKeyboardButton(text=text.strip(), url=url.strip(' \n'))
+        keyboard.add(url_button)
+
+    bot.send_message(message.chat.id, 'A list of courses:', reply_markup=keyboard)
+
+
+@bot.message_handler(commands=['schedule'])
+def list_of_courses(message):
+    res = 'Schedule of courses:\n\n'
+
+    for category in courses_plan:
+        for item in courses_plan[category]:
+            title, date = item.split(',')
+            res += f'<b>{title}</b>: <code>{date}</code>'
+        res += '\n'
+
+    bot.send_message(message.chat.id, text=res, parse_mode='HTML')
+
+@bot.message_handler(func=lambda x: x.text.startswith('info'))
+def get_course_info(message):
+    text_from_user = message.json['text']
+    if 'python' in text_from_user.lower():
+        res = ''
+        for category in courses_plan:
+            for item in courses_plan[category]:
+                title, date = item.split(',')
+                if 'python' in title.lower():
+                    res += f'<b>{title}</b>: <code>{date}</code>'
+        res += '\n'
+        bot.send_message(message.chat.id, text=res, parse_mode='HTML')
+    elif 'java' in text_from_user.lower():
+        res = ''
+        for category in courses_plan:
+            for item in courses_plan[category]:
+                title, date = item.split(',')
+                if 'java' in title.lower():
+                    res += f'<b>{title}</b>: <code>{date}</code>'
+        res += '\n'
+        bot.send_message(message.chat.id, text=res, parse_mode='HTML')
+    else:
+        bot.send_message(message.chat.id, f'I don\'t understand you')
+
+# /courses -
+# /schedule -
+
+
+if __name__ == '__main__':
+    bot.polling(none_stop=True, interval=0)
